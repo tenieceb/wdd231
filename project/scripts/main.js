8fa2552814217d3285ccb9ebf6b461d09e65be32
tenieceb <tabusenbark@gmail.com>
2025-06-10 19:16:44 -0600

    siteplan made up

---
+++ project/scripts/main.js
@@ -0,0 +1,15 @@
+//Get the current year and last modified date
+const currentyear = document.querySelector("#currentyear");
+const lastupdated = document.querySelector("#lastModified");
+const today = new Date();
+currentyear.innerHTML =`${today.getFullYear()}`;
+let lastModif = new Date(document.lastModified);
+
+const lastModifFormatted = lastModif.toLocaleDateString('en-US', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric'
+}
+)
+
+lastupdated.innerHTML = `Last Modified:  ${lastModifFormatted} ${lastModif.toLocaleTimeString()}`;
